999b1ffc188f26013c3f519fcd0c656425673257
Disha Gupta <71761740+dg-029@users.noreply.github.com>
2020-12-29 23:28:46 +0530

    Add files via upload
    A bracket sequence is called regular if it is possible to obtain correct arithmetic expression by inserting characters + and 1 into this sequence. For example, sequences (())(), () and (()(())) are regular, while )(, (() and (()))( are not. Let's call a regular bracket sequence "RBS".
    
    You are given a sequence s of n characters (, ), and/or ?. There is exactly one character ( and exactly one character ) in this sequence.
    
    You have to replace every character ? with either ) or ( (different characters ? can be replaced with different brackets). You cannot reorder the characters, remove them, insert other characters, and each ? must be replaced.
    
    Determine if it is possible to obtain an RBS after these replacements.
    
    Input
    The first line contains one integer t (1≤t≤1000) — the number of test cases.
    
    Each test case consists of one line containing s (2≤|s|≤100) — a sequence of characters (, ), and/or ?. There is exactly one character ( and exactly one character ) in this sequence.
    
    Output
    For each test case, print YES if it is possible to obtain a regular bracket sequence, or NO otherwise}.

---
+++ 1469A Regular Bracket Sequence.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+     int t;
+     cin>>t;
+     while(t--)
+     {
+          string s;
+          cin>>s;
+          int n = s.size();
+          if(n%2)
+          cout<<"NO";
+          else 
+          {
+               if(s[0]==')' || s[n-1]=='(')
+               cout<<"NO";
+               
+               else
+               cout<<"YES";
+               
+          }
+          
+          cout<<endl;
+     }
+}
